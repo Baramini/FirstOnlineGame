9f408f2be05728048ec62301230fd32bfe520efd
Baramini <145976578+Baramini@users.noreply.github.com>
2025-04-05 18:54:44 +0900

    1, 2week

---
+++ Assets/02.Scripts/Manager/ResourceManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UltimateCartFights.Utility {
+    public class ResourceManager : MonoBehaviour {
+        [Header("Network Objects")]
+        public GameObject Session;
+
+        [Header("Character Sprites")]
+        public List<Sprite> Characters;
+
+        [Header("Cart Color Material")]
+        public List<Material> ColorMaterials;
+
+        public static ResourceManager Instance = null;
+
+        private void Awake() {
+            if(Instance) {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
